5439fba407c41114fdc8f009d05364cb312d57ee
kraventh <kraventh@gmail.com>
2015-08-09 15:59:50 +0200

    Draft - 20150809M

--- app/src/main/java/es/dlacalle/finalpfg/MainActivity.java
+++ app/src/main/java/es/dlacalle/finalpfg/MainActivity.java
@@ -1,22 +1,106 @@
 package es.dlacalle.finalpfg;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    private float x1, x2, lastX;
+    static final int MIN_DISTANCE = 150;
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new SettingsFragment())
-                .commit();
-
+        //viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
     }
 
+    // Method to handle touch event like left to right swap and right to left swap
+//    public boolean onTouchEvent(MotionEvent touchevent)
+//    {
+//        switch (touchevent.getAction())
+//        {
+//            // when user first touches the screen to swap
+//            case MotionEvent.ACTION_DOWN:
+//            {
+//                lastX = touchevent.getX();
+//                break;
+//            }
+//            case MotionEvent.ACTION_UP:
+//            {
+//                float currentX = touchevent.getX();
+//
+//                // if left to right swipe on screen
+//                if (lastX < currentX && Math.abs(lastX-currentX)>150 )
+//                {
+//                    // If no more View/Child to flip
+//                    if (viewFlipper.getDisplayedChild() == 0)
+//                        break;
+//
+//                    // set the required Animation type to ViewFlipper
+//                    // The Next screen will come in form Left and current Screen will go OUT from Right
+//                    viewFlipper.setInAnimation(this, R.anim.in_from_left);
+//                    viewFlipper.setOutAnimation(this, R.anim.out_to_right);
+//                    // Show the next Screen
+//                    viewFlipper.showNext();
+//                }
+//
+//                // if right to left swipe on screen
+//                if (lastX > currentX && Math.abs(lastX-currentX)>150)
+//                {
+//                    if (viewFlipper.getDisplayedChild() == 1)
+//                        break;
+//                    // set the required Animation type to ViewFlipper
+//                    // The Next screen will come in form Right and current Screen will go OUT from Left
+//                    viewFlipper.setInAnimation(this, R.anim.in_from_right);
+//                    viewFlipper.setOutAnimation(this, R.anim.out_to_left);
+//                    // Show The Previous Screen
+//                    viewFlipper.showPrevious();
+//                }
+//                break;
+//            }
+//        }
+//        return false;
+//    }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                x1 = event.getX();
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                x2 = event.getX();
+//                float deltaX = x2 - x1;
+//
+//                if (Math.abs(deltaX) > MIN_DISTANCE) {
+//                    // Left to Right swipe action
+//                    if (x2 > x1) {
+//                        Toast.makeText(this, "Left to Right swipe [Next]", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    // Right to left swipe action
+//                    else {
+//                        Toast.makeText(this, "Right to Left swipe [Previous]", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                } else {
+//                    // consider as something else - a screen tap for example
+//                }
+//                break;
+//        }
+//        return super.onTouchEvent(event);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,10 +118,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new SettingsFragment())
-                    .addToBackStack(null)
-                    .commit();
+
             return true;
         }
 
--- app/src/main/java/es/dlacalle/finalpfg/SettingsFragment.java
+++ app/src/main/java/es/dlacalle/finalpfg/SettingsFragment.java
@@ -1,22 +1,9 @@
 package es.dlacalle.finalpfg;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.preference.DialogPreference;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceGroup;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 
@@ -25,22 +12,12 @@ import android.widget.Toast;
  */
 public class SettingsFragment extends PreferenceFragment {
 
-    private final int REQUEST_ENABLE_BT = 0;
-    BluetoothAdapter mBluetoothAdapter;
-
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         //Cargar las preferencias desde el XML
         addPreferencesFromResource(R.xml.preferences);
-
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-            Toast.makeText(getActivity().getBaseContext(), "Bluetooth no encontrado", Toast.LENGTH_SHORT).show();
-        }
 
         // Monitoriza cambios en EditTextPreference
         // para guardar los cambios en un fichero.log si se pulsa Guardar
